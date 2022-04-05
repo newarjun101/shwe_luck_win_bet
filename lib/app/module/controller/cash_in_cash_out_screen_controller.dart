c6571c416a20e9f71ccf847f481ce6007877b6af
prolaymm <prolaymmm@gmail.com>
2022-04-05 10:29:59 +0630

    added snack bar on cash in cash out

--- lib/app/module/controller/cash_in_cash_out_screen_controller.dart
+++ lib/app/module/controller/cash_in_cash_out_screen_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:shwe_luck_win_bet/app/core/constants/default_values.dart';
 import 'package:shwe_luck_win_bet/app/core/local_%20widget/custom_buttom_sheet.dart';
+import 'package:shwe_luck_win_bet/app/core/local_%20widget/custom_snack_bar.dart';
 import 'package:shwe_luck_win_bet/app/core/local_%20widget/payment_type_list_widget.dart';
 
 import '../../core/data/model/payment_type_model.dart';
@@ -14,26 +15,35 @@ import '../../core/local_ widget/custom_dialog.dart';
 class CashInCashOutScreenController extends GetxController {
   late PaymentRepo _paymentRepo;
   RxList<PaymentTypeModel> mPaymentList = RxList([]);
-  late int mSelectedPaymentIndex =-1;
+  late int mSelectedPaymentIndex = -1;
   RxBool isPaymentSelected = false.obs;
   RxBool haveLoading = false.obs;
+  RxString errorMessage = RxString("");
 
   showPaymentTypeWidget(context) {
-    Future.delayed(Duration(milliseconds: 100), () {
+    Future.delayed(const Duration(milliseconds: 100), () {
       customBottomSheet(
-          context: context, child: PaymentTypeListWidget(), height: 0.31);
+          context: context, child: const PaymentTypeListWidget(), height: 0.31);
     });
   }
 
   getAllPayment() async {
+    haveLoading.value = true;
     try {
       ApiResult<List<PaymentTypeModel>> result =
           await _paymentRepo.getPaymentType();
       if (result.status == Status.eCOMPLETED) {
         debugPrint(result.mData.length.toString());
         mPaymentList.value = result.mData;
-      } else {}
+        haveLoading.value = false;
+        errorMessage.value = "Success";
+      } else {
+        haveLoading.value = false;
+        errorMessage.value = result.errorMessage;
+      }
     } catch (e) {
+      haveLoading.value = false;
+      errorMessage.value = "Error On Payment Request";
       print("catch erron on cash in cahs out controller");
     }
   }
@@ -66,13 +76,36 @@ class CashInCashOutScreenController extends GetxController {
       ApiResult result = await _paymentRepo.cashInRequest(test);
       if (result.status == Status.eCOMPLETED) {
         haveLoading.value = false;
+        errorMessage.value = "Success";
         Get.back();
+        customSnackBar(
+            title: "Payment Successful",
+            icon: Icons.check_circle,
+            snackPosition: SnackPosition.BOTTOM,
+            bgColor: Colors.white,
+            iconColor: Colors.white);
       } else {
         Get.back();
         haveLoading.value = false;
+        errorMessage.value = result.errorMessage;
+        haveLoading.value = false;
+        customSnackBar(
+            title: "Error on Payment",
+            icon: Icons.check_circle,
+            snackPosition: SnackPosition.BOTTOM,
+            bgColor: Colors.white,
+            iconColor: Colors.white);
       }
     } catch (e) {
       Get.back();
+      haveLoading.value = false;
+      errorMessage.value = "Something wrong with Server";
+      customSnackBar(
+          title: "Error on Payment",
+          icon: Icons.check_circle,
+          snackPosition: SnackPosition.BOTTOM,
+          bgColor: Colors.white,
+          iconColor: Colors.white);
       debugPrint(e.toString());
     }
   }
@@ -90,22 +123,48 @@ class CashInCashOutScreenController extends GetxController {
       "user_phone": phone
     };
     customDialog(
-        context, "Loading",const Center (
-
-        child:SizedBox (      width: 20,
-            height: 20,child: CircularProgressIndicator())));
+        context,
+        "Loading",
+        const Center(
+            child: SizedBox(
+                width: 20, height: 20, child: CircularProgressIndicator())));
     haveLoading = true.obs;
     try {
       ApiResult result = await _paymentRepo.cashOutRequest(body);
       if (result.status == Status.eCOMPLETED) {
         haveLoading.value = false;
         Get.back();
+        errorMessage.value = "Success";
+        customSnackBar(
+            title: "Payment Request Successful",
+            icon: Icons.check_circle,
+            snackPosition: SnackPosition.BOTTOM,
+            bgColor: Colors.white,
+            iconColor: Colors.white);
+
       } else {
         Get.back();
         haveLoading.value = false;
+        errorMessage.value = result.errorMessage;
+        Get.back();
+        customSnackBar(
+            title: "Error on Payment",
+            icon: Icons.check_circle,
+            snackPosition: SnackPosition.BOTTOM,
+            bgColor: Colors.white,
+            iconColor: Colors.white);
       }
     } catch (e) {
       Get.back();
+      haveLoading.value = false;
+      errorMessage.value = "Something wrong with Server";
+      Get.back();
+      customSnackBar(
+          title: "Error on Payment",
+          icon: Icons.check_circle,
+          snackPosition: SnackPosition.BOTTOM,
+          bgColor: Colors.white,
+          iconColor: Colors.white);
       debugPrint(e.toString());
     }
   }
